4c6c2a4e9cdd04e89a4c2d91058fb74759d657ae
salonikumari12 <162488452+salonikumari12@users.noreply.github.com>
2025-06-11 18:36:51 +0530

    Update Signup.js

--- src/component/src/component/Signup.js
+++ src/component/src/component/Signup.js
@@ -1,1 +1,18 @@
+import React from 'react';
 
+const SignUp = () => {
+  return (
+    <div className="signup">
+      <h2>Sign Up</h2>
+      {/* Placeholder form */}
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;

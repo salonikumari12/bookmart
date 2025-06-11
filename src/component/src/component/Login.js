84e97c8739238a86ded951fee064aa93d47704ef
salonikumari12 <162488452+salonikumari12@users.noreply.github.com>
2025-06-11 18:34:20 +0530

    Update Login.js

--- src/component/src/component/Login.js
+++ src/component/src/component/Login.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+const Login = () => {
+  return (
+    <div className="login">
+      <h2>Login</h2>
+      {/* Placeholder form */}
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

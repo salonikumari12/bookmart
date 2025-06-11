640d82a2f3fd9621efa786b748cb636b46aa91e3
salonikumari12 <162488452+salonikumari12@users.noreply.github.com>
2025-06-11 18:25:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import React from 'react';More actions
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import SellerDashboard from './components/SellerDashboard';
+import BuyerDashboard from './components/BuyerDashboard';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>📚 Welcome to BookMart 📚</h1>
+      {/* Placeholder to render components */}
+      <SignUp />
+      <Login />
+      <SellerDashboard />
+      <BuyerDashboard />
+    </div>
+  );
+}
+
+export default App;
